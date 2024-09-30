19c3a85830033a7fa450521ae08f8feb118a677a
Manas Mengle <manmenonsense@gmail.com>
2024-09-30 19:28:47 +0100

    switch from classroom to moodle

--- config.ts
+++ config.ts
@@ -5,76 +5,76 @@
 //
 //    https://github.com/migueravila/Bento - upstream
 
-import { Config } from "./src/types/config";
+import { Config } from './src/types/config';
 
 // Bento-next config
 // Use an editor with Typescript support to get autocomplete and validity checking!
 export const config: Config = {
   // General
-  name: "Manas",
+  name: 'Manas',
   openInNewTab: true,
-  title: "Bento",
+  title: 'Bento',
 
   // Clock
   twelveHourFormat: false,
   flashSeparator: false,
 
   // Theme
-  theme: "catppuccin-frappe",
+  theme: 'catppuccin-frappe',
 
   // Place a background image in ./src/assets/images/ and provide the file name.
   // Alternatively, provide a URL to an image. If the page is served over https, you may have issues loading images from insecure origins.
   // Set to "" to disable.
-  backgroundImage: "",
+  backgroundImage: '',
   themes: [
     // List of installed themes, add your own themes in ./src/assets/css/themes and include it in the array below.
-    "arc",
-    "bento",
-    "catppuccin-frappe",
-    "catppuccin-macchiato",
-    "catppuccin-mocha",
-    "conceptdark", // dark mode only
-    "monokai",
-    "nord",
-    "sakura",
-    "solarized",
+    'arc',
+    'bento',
+    'catppuccin-frappe',
+    'catppuccin-macchiato',
+    'catppuccin-mocha',
+    'conceptdark', // dark mode only
+    'monokai',
+    'nord',
+    'sakura',
+    'solarized',
   ],
 
   // Search Bar
   searchBar: true,
-  searchEngine: "ddg", // google, ddg
-  barPlaceholder: "", // if blank, use search engine name. Set to ' ' for no placeholder.
+  searchEngine: 'ddg', // google, ddg
+  barPlaceholder: '', // if blank, use search engine name. Set to ' ' for no placeholder.
   autoFocusBar: true,
 
   // Greetings
-  greetingMorning: "Good morning,",
-  greetingAfternoon: "Good afternoon,",
-  greetingEvening: "Good evening,",
-  greetingNight: "Sweet dreams,",
+  greetingMorning: 'Good morning,',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Sweet dreams,',
 
   // Layout
-  layout: "bento", // 'bento', 'lists', 'buttons'
+  layout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
   weatherKey: import.meta.env.VITE_API_KEY, // Set your OpenWeatherMap key in .env - Move the included .env.example to .env
-  weatherIcons: "OneDark", // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: "C", // 'F', 'C'
-  language: "en", // More languages in https://openweathermap.org/current#multi
+  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // Request location from the browser. If false, or location is denied, use the coordinates below.
-  defaultLatitude: "37.775",
-  defaultLongitude: "-122.419",
+  defaultLatitude: '37.775',
+  defaultLongitude: '-122.419',
 
   // Automatic theme switching:
   // "system" - Switches based on OS color preference
   // "location" - Switches based on local sunrise/sunset (requires OpenWeatherMap API key)
   // "preset" - Switches based on set hours
   // "none" - No automatic switching
-  autoTheme: "system",
+  autoTheme: 'system',
 
   // If autoTheme is set to "preset", set the hours below.
-  darkModeOnTime: "18:30",
-  lightModeOnTime: "07:00",
+  darkModeOnTime: '18:30',
+  lightModeOnTime: '07:00',
 
   // Set any of the below options to false to disable drawing the component on the page.
   // If themeButton is set to false, the theme can only be set in this file.
@@ -94,68 +94,68 @@ export const config: Config = {
     // If you're using the 'bento' layout, this is the only group that will be used.
     [
       {
-        name: "Github", // set the name of the card
-        icon: "fab-github", // provide a FontAwesome icon name. Regular icons (fa-) don't require a prefix, while FA-Brands icons must be prefixed with "fab-"
-        url: "https://github.com/", // provide a link to the page.
+        name: 'Github', // set the name of the card
+        icon: 'fab-github', // provide a FontAwesome icon name. Regular icons (fa-) don't require a prefix, while FA-Brands icons must be prefixed with "fab-"
+        url: 'https://github.com/', // provide a link to the page.
       },
       {
-        name: "Mail",
-        icon: "envelope",
-        url: "https://mail.google.com/",
+        name: 'Mail',
+        icon: 'envelope',
+        url: 'https://mail.google.com/',
       },
       {
-        name: "Classroom",
-        icon: "list-ol",
-        url: "https://classroom.google.com/a/not-turned-in/all",
+        name: 'Moodle',
+        icon: 'list-ol',
+        url: 'https://moodle.bath.ac.uk/',
       },
       {
-        name: "Calendar",
-        icon: "calendar",
-        url: "https://calendar.google.com/calendar/r",
+        name: 'Calendar',
+        icon: 'calendar',
+        url: 'https://calendar.google.com/calendar/r',
       },
       {
-        name: "Reddit",
-        icon: "fab-reddit",
-        url: "https://reddit.com",
+        name: 'Reddit',
+        icon: 'fab-reddit',
+        url: 'https://reddit.com',
       },
       {
-        name: "Odysee",
-        icon: "fab-youtube",
-        url: "https://youtube.com/",
+        name: 'Odysee',
+        icon: 'fab-youtube',
+        url: 'https://youtube.com/',
       },
     ],
     // Second buttons group
     // You muse use the 'buttons' layout to use this group.
     [
       {
-        name: "Music",
-        icon: "fab-spotify",
-        url: "https://open.spotify.com",
+        name: 'Music',
+        icon: 'fab-spotify',
+        url: 'https://open.spotify.com',
       },
       {
-        name: "twitter",
-        icon: "fab-twitter",
-        url: "https://twitter.com/",
+        name: 'twitter',
+        icon: 'fab-twitter',
+        url: 'https://twitter.com/',
       },
       {
-        name: "bot",
-        icon: "fab-discord",
-        url: "https://discord.com/app",
+        name: 'bot',
+        icon: 'fab-discord',
+        url: 'https://discord.com/app',
       },
       {
-        name: "Last.fm",
-        icon: "fab-amazon",
-        url: "https://last.fm/",
+        name: 'Last.fm',
+        icon: 'fab-amazon',
+        url: 'https://last.fm/',
       },
       {
-        name: "Hashnode",
-        icon: "fab-hashnode",
-        url: "https://hashnode.com/",
+        name: 'Hashnode',
+        icon: 'fab-hashnode',
+        url: 'https://hashnode.com/',
       },
       {
-        name: "Figma",
-        icon: "fab-figma",
-        url: "https://figma.com/",
+        name: 'Figma',
+        icon: 'fab-figma',
+        url: 'https://figma.com/',
       },
     ],
   ],
@@ -166,44 +166,44 @@ export const config: Config = {
     // If you're using the 'bento' layout, this is the only group that will be used.
     [
       {
-        icon: "share-nodes", // Icon for the list to use. The same as the buttons.
+        icon: 'share-nodes', // Icon for the list to use. The same as the buttons.
         links: [
           {
-            name: "Spotify", // Display text for the link
-            url: "https://www.spotify.com", // URL to the page
+            name: 'Spotify', // Display text for the link
+            url: 'https://www.spotify.com', // URL to the page
           },
           {
-            name: "Reddit",
-            url: "https://www.reddit.com",
+            name: 'Reddit',
+            url: 'https://www.reddit.com',
           },
           {
-            name: "Tumblr",
-            url: "https://www.tumblr.com",
+            name: 'Tumblr',
+            url: 'https://www.tumblr.com',
           },
           {
-            name: "Hacker News",
-            url: "https://news.ycombinator.com",
+            name: 'Hacker News',
+            url: 'https://news.ycombinator.com',
           },
         ],
       },
       {
-        icon: "book",
+        icon: 'book',
         links: [
           {
-            name: "Core Pure 1",
-            url: "https://www.physicsandmathstutor.com/maths-revision/solutionbanks/edexcel-core-pure-maths-1/",
+            name: 'Core Pure 1',
+            url: 'https://www.physicsandmathstutor.com/maths-revision/solutionbanks/edexcel-core-pure-maths-1/',
           },
           {
-            name: "Stats & Mech Y2",
-            url: "https://www.physicsandmathstutor.com/maths-revision/solutionbanks/edexcel-statistics-mechanics-year-2/",
+            name: 'Stats & Mech Y2',
+            url: 'https://www.physicsandmathstutor.com/maths-revision/solutionbanks/edexcel-statistics-mechanics-year-2/',
           },
           {
-            name: "Notion",
-            url: "https://www.notion.so",
+            name: 'Notion',
+            url: 'https://www.notion.so',
           },
           {
-            name: "Slack",
-            url: "https://www.slack.com",
+            name: 'Slack',
+            url: 'https://www.slack.com',
           },
         ],
       },
@@ -212,44 +212,44 @@ export const config: Config = {
     // You muse use the 'lists' layout to use this group.
     [
       {
-        icon: "house-user",
+        icon: 'house-user',
         links: [
           {
-            name: "Spotify",
-            url: "https://www.spotify.com",
+            name: 'Spotify',
+            url: 'https://www.spotify.com',
           },
           {
-            name: "Reddit",
-            url: "https://www.reddit.com",
+            name: 'Reddit',
+            url: 'https://www.reddit.com',
           },
           {
-            name: "Hashnode",
-            url: "https://www.hashnode.com",
+            name: 'Hashnode',
+            url: 'https://www.hashnode.com',
           },
           {
-            name: "Pocket",
-            url: "https://www.pocket.com",
+            name: 'Pocket',
+            url: 'https://www.pocket.com',
           },
         ],
       },
       {
-        icon: "fab-github",
+        icon: 'fab-github',
         links: [
           {
-            name: "Front",
-            url: "https://www.reddit.com/r/Frontend/",
+            name: 'Front',
+            url: 'https://www.reddit.com/r/Frontend/',
           },
           {
-            name: "Rust",
-            url: "https://www.reddit.com/r/rust/",
+            name: 'Rust',
+            url: 'https://www.reddit.com/r/rust/',
           },
           {
-            name: "Go",
-            url: "https://www.reddit.com/r/golang/",
+            name: 'Go',
+            url: 'https://www.reddit.com/r/golang/',
           },
           {
-            name: "Repos",
-            url: "https://github.com/appcreatorguy",
+            name: 'Repos',
+            url: 'https://github.com/appcreatorguy',
           },
         ],
       },
